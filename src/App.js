5eb175ed25e838a0f6c1d8491a03cce99de598af
clOUdy <riyanshverma@jklu.edu.in>
2024-11-05 10:56:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
