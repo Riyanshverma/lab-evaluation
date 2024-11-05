5da6c37ec5a126a15141cb9633efc8650f3cbfaf
clOUdy <riyanshverma@jklu.edu.in>
2024-11-05 11:31:45 +0530

    final submit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,58 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel, Card } from 'react-bootstrap';
 
-function Home(){
-    return(
-        <div>
-            <h2>Welcome to the home page.</h2>
-            <p>This is the portected page accessible after login.</p>
-            <h2>It is the h2 heading.</h2>
-        </div>
-    )
+function Home() {
+    return (
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col">
+                        <Card>
+                            <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                            <Card.Body>
+                                <Card.Title>Card Title 1</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                            <Card.Body>
+                                <Card.Title>Card Title 2</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                            <Card.Body>
+                                <Card.Title>Card Title 3</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div className="col">
+                        <Card>
+                            <Card.Img variant="top" src="https://via.placeholder.com/150" />
+                            <Card.Body>
+                                <Card.Title>Card Title 4</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+    );
 }
 
-export default Home
+export default Home;
