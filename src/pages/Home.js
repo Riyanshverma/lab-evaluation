69ea0a55becfca1cdf4704d4b04f6eb6aabedc1c
clOUdy <riyanshverma@jklu.edu.in>
2024-11-05 10:59:11 +0530

    added h2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ function Home(){
         <div>
             <h2>Welcome to the home page.</h2>
             <p>This is the portected page accessible after login.</p>
+            <h2>It is the h2 heading.</h2>
         </div>
     )
 }
